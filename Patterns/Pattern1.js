6b186e12509da8137d953a7e9302a226a6362468
Akshay <akshay3753@gmail.com>
2025-08-03 15:26:07 +0530

    Pattern Problem-1

---
+++ Patterns/Pattern1.js
@@ -0,0 +1,13 @@
+function pattern1(n) {
+    
+    for (let i = 1; i <= n; i++ ){
+        let row = "";
+        for (j = 1; j <= n; j++){
+             row += "* ";
+        }
+        console.log(row);
+    }
+    
+}
+
+pattern1(5);
