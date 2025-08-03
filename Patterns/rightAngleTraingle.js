34113aa77f23858b021add8c0740de630b3855fc
Akshay <akshay3753@gmail.com>
2025-08-03 16:58:26 +0530

    Right angle triangle

---
+++ Patterns/rightAngleTraingle.js
@@ -0,0 +1,20 @@
+function pattern2(n){
+    for (let i = 1; i <= n; i++){
+        let row = "";
+        for (j = 1; j <= i; j++){
+            row += "*";
+        }
+        console.log(row);
+    }
+}
+pattern2(4);
+
+// *
+
+// **
+
+// ***
+
+// ****
+
+// *****
